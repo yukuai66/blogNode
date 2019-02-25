e99f9d5e9632e232db6935ded79c2ee72ab5a49c
yukuai <qingyu.yk.raycloud.com>
2019-02-25 17:20:54 +0800

    修改了 session过期时间

--- config/default.js
+++ config/default.js
@@ -3,7 +3,7 @@ module.exports = {
   session: {
     secret: 'myblog',
     key: 'myblog',
-    maxAge: 2592000000
+    maxAge: 86400000
   },
   mongodb: {
     url: 'mongodb://127.0.0.1:27017/myblog',
